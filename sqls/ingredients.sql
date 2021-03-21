1d8c5c2d038e3791c59102d889bab5215745a5cb
gerMaAg <gerardma90@outlook.com>
2021-03-21 13:21:12 +0100

    Primera pujada

---
+++ sqls/ingredients.sql
@@ -0,0 +1,42 @@
+CREATE TABLE `ingredient` (
+`id` TEXT NOT NULL,
+`nom` TEXT, 
+`descripcio` TEXT);
+
+INSERT INTO `ingredient` (`id`,`nom`, `descripcio` ) VALUES ('wyvern_hide', 'Wyvern hide', 'Wyvern hide is an alchemy ingredient in The Witcher 3: Wild Hunt but is not needed for anything in particular. It can be dismantled into a Monster carapace and Chitinous shell.');
+INSERT INTO `ingredient` (`id`,`nom`, `descripcio` ) VALUES ('albedo', 'Albedo', 'A craftable ingredient used in alchemy');
+INSERT INTO `ingredient` (`id`,`nom`, `descripcio` ) VALUES ('wyvern_mutagen', 'Wyvern mutagen', 'Wyvern mutagen is an alchemy ingredient in The Witcher 3: Wild Hunt and is used to craft wyvern decoctions. They act like lesser red mutagens when placed in the diamond-shaped slots in the character panel.');
+INSERT INTO `ingredient` (`id`,`nom`, `descripcio` ) VALUES ('alcohest', 'Alcohest', 'A craftable ingredient used in alchemy');
+INSERT INTO `ingredient` (`id`,`nom`, `descripcio` ) VALUES ('bryonia', 'Bryonia', 'The stems of the bryonia are used in alchemy. It is a plant which grows in wastelands.');
+INSERT INTO `ingredient` (`id`,`nom`, `descripcio` ) VALUES ('arenaria', 'Arenaria', 'A craftable ingredient used in alchemy');
+INSERT INTO `ingredient` (`id`,`nom`, `descripcio` ) VALUES ('aether', 'Aether', 'Aether, indicated in purple, is a basic alchemical substance. It is found in many ingredients, including herbs, minerals and monster parts. This category contains all ingredients with aether as their primary substance.');
+INSERT INTO `ingredient` (`id`,`nom`, `descripcio` ) VALUES ('archgriffin', 'Archgriffin mutagen', 'They act like lesser red mutagens when placed in the diamond-shaped slots in the character panel.');
+INSERT INTO `ingredient` (`id`,`nom`, `descripcio` ) VALUES ('arachas_eyes', 'Arachas eyes', 'They can be broken down into a monster eye. Unless one is desperate for monster eyes (unlikely), it makes the wisest economic sense to sell these to an alchemist or herbalist vendor rather than dismantle them.');
+INSERT INTO `ingredient` (`id`,`nom`, `descripcio` ) VALUES ('harpy_eggs', 'Harpy Eggs', 'A craftable ingredient used in alchemy');
+INSERT INTO `ingredient` (`id`,`nom`, `descripcio` ) VALUES ('longrube', 'Longrube', 'Longrube is an alchemy ingredient in The Witcher 3: Wild Hunt');
+INSERT INTO `ingredient` (`id`,`nom`, `descripcio` ) VALUES ('water_essence', 'Water essence', 'Water essence is an alchemy ingredient in The Witcher 3: Wild Hunt and can be dismantled into monster essence.');
+INSERT INTO `ingredient` (`id`,`nom`, `descripcio` ) VALUES ('ducal_water', 'Ducal water', 'A craftable ingredient used in alchemy');
+INSERT INTO `ingredient` (`id`,`nom`, `descripcio` ) VALUES ('balisse_fruit', 'Balise fruit', 'Balisse fruit is an edible berry characterized by subtle magic resonance. The fruit is apparently poisonous unless soaked in alcohol first, after which it can be used for medicines.');
+INSERT INTO `ingredient` (`id`,`nom`, `descripcio` ) VALUES ('alchemists_powder', 'Alchemists'' powder', 'Alchemists'' powder is an alchemy ingredient in The Witcher 3: Wild Hunt that is needed to craft the following items');
+INSERT INTO `ingredient` (`id`,`nom`, `descripcio` ) VALUES ('foglet_teeth', 'Foglet teeth', 'Foglet teeth is an alchemy ingredient in The Witcher 3: Wild Hunt. They can be dismantled into monster teeth, and are needed to craft the following items:');
+INSERT INTO `ingredient` (`id`,`nom`, `descripcio` ) VALUES ('nilfgaardian_lemon', 'Nilfgaardian Lemon', 'Nilfgaardian lemon is considered an alchemy ingredient in The Witcher 3: Wild Hunt and thus can''t be consumed like regular drinks. It''s needed to craft alcohest.');
+INSERT INTO `ingredient` (`id`,`nom`, `descripcio` ) VALUES ('coacktrice_egg', 'Coacktrice egg', 'Cockatrice egg is an alchemy ingredient in The Witcher 3: Wild Hunt but is not needed to craft anything. It can be dismantled into a Monster egg and Chitinous shell.');
+
+
+INSERT INTO `ingredient` (`id`,`nom`, `descripcio` ) VALUES ('troll_hide', 'Troll hide', 'Troll hide is an alchemy ingredient in The Witcher 3: Wild Hunt but is not needed to craft anything. It can be dismantled into monster hide and monster hair.');
+INSERT INTO `ingredient` (`id`,`nom`, `descripcio` ) VALUES ('han_fiber', 'Han Fiber', 'Han is an alchemy ingredient that can be found throughout the fields surrounding Murky Waters.');
+INSERT INTO `ingredient` (`id`,`nom`, `descripcio` ) VALUES ('necrophage_hide', 'Necrophage hide', 'Necrophage hide is an alchemy ingredient but is not needed to craft anything. It can be dismantled into monster hide and monster blood.');
+INSERT INTO `ingredient` (`id`,`nom`, `descripcio` ) VALUES ('fools_parsley_leaves', 'Fool''s parsley leaves', 'Fool''s parsley is a swamp plant with fleshy leaves. It is abundant in both the swamp and the swamp cemetery. The leaves of this plant.');
+
+
+INSERT INTO `ingredient` (`id`,`nom`, `descripcio` ) VALUES ('harpy_egg', 'Harpy eggs', 'This item is dropped by the queen harpy and other kinds of harpies.');
+INSERT INTO `ingredient` (`id`,`nom`, `descripcio` ) VALUES ('specter_dust', 'Specter dust', 'Can be dismantled into powdered monster tissue');
+INSERT INTO `ingredient` (`id`,`nom`, `descripcio` ) VALUES ('powdered_monster_tissue', 'Powdered monster tissue', 'Can be obtained from certain monsters or by dismantling some items');
+INSERT INTO `ingredient` (`id`,`nom`, `descripcio` ) VALUES ('ekimmara_hide', 'Ekimmara hide', 'Ekhidna mutagen is an alchemy ingredient in The Witcher 3: Wild Hunt that is used to craft ekhidna decoction. They act like lesser blue mutagens when placed in the diamond-shaped slots in the character panel.');
+INSERT INTO `ingredient` (`id`,`nom`, `descripcio` ) VALUES ('ekimmara_mutagen', 'Ekimmara mutagen', 'Ekimmara mutagen is an alchemy ingredient and is used to craft ekimmara decoctions. They act like lesser green mutagens when placed in the diamond-shaped slots in the character panel.');
+INSERT INTO `ingredient` (`id`,`nom`, `descripcio` ) VALUES ('elemental_essence', 'Elemental essence', 'Elemental essence is an alchemy ingredient in The Witcher 3: Wild Hunt but is not needed for anything. It can be dismantled into monster essence.');
+INSERT INTO `ingredient` (`id`,`nom`, `descripcio` ) VALUES ('empty_bottle', 'Empty bottle', 'Empty bottle is an alchemy ingredient in The Witcher 3: Wild Hunt that can be dismantled into glass.');
+INSERT INTO `ingredient` (`id`,`nom`, `descripcio` ) VALUES ('bear_fat', 'Bear Fat', 'Special fat extracted from bear fur that helps lit fire');
+INSERT INTO `ingredient` (`id`,`nom`, `descripcio` ) VALUES ('cofee', 'Cofee', 'High-power stimulant, absolutely required in programming warfare campaings.');
+
+
